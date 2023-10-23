da1d7e656dfbd6e85581173179bc3967b37f81ca
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-10-23 16:26:34 +0300

    get results of test

--- src/main/java/com/exam/repo/AnswerRepository.java
+++ src/main/java/com/exam/repo/AnswerRepository.java
@@ -3,5 +3,9 @@ package com.exam.repo;
 import com.exam.models.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionIdAndIsCorrectTrue(Long questionId);
+
 }
