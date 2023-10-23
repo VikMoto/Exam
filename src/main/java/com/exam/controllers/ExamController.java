af55ed3eee3640618717698cb170fa5686fbb649
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-10-23 15:31:36 +0300

    get question one by one

--- src/main/java/com/exam/controllers/ExamController.java
+++ src/main/java/com/exam/controllers/ExamController.java
@@ -3,18 +3,21 @@ package com.exam.controllers;
 import com.exam.models.Question;
 import com.exam.models.User;
 import com.exam.services.ExamService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/exam")
 public class ExamController {
-
 
     private final ExamService examService;
 
@@ -27,7 +30,6 @@ public class ExamController {
         return "index";
     }
 
-
     @PostMapping("/register")
     public String register(User user, Model model) {
         examService.saveUser(user);
@@ -36,23 +38,65 @@ public class ExamController {
     }
 
     @GetMapping("/start")
-    public String startExam(@ModelAttribute User user, Model model) {
-        List<Question> questions = examService.getAllQuestions();
-        model.addAttribute("questions", questions);
-        model.addAttribute("currentUser", user);
-        return "step4";  // Assuming step4.html displays the questions.
+    public String startExam(Model model) {
+        User user = examService.getLatestRegisteredUser();
+        Question firstQuestion = examService.getFirstQuestion();
+
+        model.addAttribute("question", firstQuestion);
+        model.addAttribute("user", user);
+
+        return "step4";
     }
+
+//    @PostMapping("/submit")
+//    public String submitAnswers(@RequestParam Long userId, @RequestParam Map<String, String> answersMap, Model model)
+//    {
+//        for (String answerString : answersMap.values()) {
+//            try {
+//                Long answerId = Long.parseLong(answerString);
+//                System.out.println("answerId = " + answerId);
+//                examService.saveSelectedAnswerForUser(userId, answerId);
+//            } catch (NumberFormatException ex) {
+//                System.err.println("Invalid answer ID format: " + answerString);
+//            }
+//        }
+//
+//        int score = examService.calculateScoreForUser(userId);
+//        model.addAttribute("score", score);
+//        return "result";
+//    }
 
     @PostMapping("/submit")
-    public String submitAnswers(@ModelAttribute User user, @RequestParam List<Long> answerIds, Model model) {
-        for (Long answerId : answerIds) {
-            examService.saveSelectedAnswerForUser(user.getId(), answerId);
+    public String submitAnswer(@RequestParam(name = "userId") Long userId,
+                               @RequestParam Map<String, String> allRequestParams,
+                               Model model) {
+
+        // Extract and process submitted answer here (you can retrieve it from allRequestParams)
+
+        // Set the current question ID for the service
+        String answerKey = allRequestParams.keySet().stream()
+                .filter(k -> k.startsWith("answerForQuestion_"))
+                .findFirst()
+                .orElse(null);
+
+        if (answerKey != null) {
+            String[] parts = answerKey.split("_");
+            Long currentQuestionId = Long.parseLong(parts[1]);
+            examService.setCurrentQuestionId(currentQuestionId);
         }
 
-        int score = examService.calculateScoreForUser(user.getId());
-        model.addAttribute("score", score);
-        return "result";  // Assuming result.html displays the user's score.
+        Question nextQuestion = examService.getNextQuestion();
+
+        if (nextQuestion == null) {
+            return "redirect:/examFinished"; // Or wherever you want to redirect when the exam is finished
+        }
+
+        model.addAttribute("question", nextQuestion);
+        model.addAttribute("user", examService.getUserById(userId)); // Assumes you have a method to get a user by ID
+
+        return "step4";
     }
+
 
     @GetMapping("/result")
     public String displayResult(@ModelAttribute User user, Model model) {
@@ -60,18 +104,16 @@ public class ExamController {
         String rating = determineRating(score);
         model.addAttribute("score", score);
         model.addAttribute("rating", rating);
-        return "step17";  // Assuming step17.html displays detailed results.
+        return "step17";
     }
 
     private String determineRating(int score) {
         if (score >= 56) return "12";
         if (score >= 51) return "11";
         if (score >= 46) return "10";
-        // ... and so on for each range.
-        return "0";  // Default if none of the above.
+        return "0";
     }
 
-    // You can add more methods as needed for the rest of the steps.
 }
 
 
--- src/main/java/com/exam/repo/QuestionRepository.java
+++ src/main/java/com/exam/repo/QuestionRepository.java
@@ -3,5 +3,12 @@ package com.exam.repo;
 import com.exam.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Question findFirstByOrderByOrderAsc();
+    List<Question> findByOrderGreaterThanOrderByOrderAsc(Integer order);
+
+    Question findFirstByOrderGreaterThanOrderByOrderAsc(Integer orderValue);
+
 }
